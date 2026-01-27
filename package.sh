a3c561d2b96f56d9f43c642584d232259bc5dd3d
wangyupo <1106774262@qq.com>
2026-01-27 22:46:57 +0800

    [^]Update package.sh

--- package.sh
+++ package.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
-echo "打包中..."
+echo "更新版本号..."
+npm version patch --no-git-tag-version
 
+VERSION=$(node -p "require('./package.json').version")
+echo "新版本: $VERSION"
+
+echo "打包中..."
 vsce package
 
-echo "打包完成！"
+echo "打包完成！生成: v3s-snippets-$VERSION.vsix"
